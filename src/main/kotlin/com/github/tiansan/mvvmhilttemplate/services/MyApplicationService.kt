186876d2e24e1a6027f30a154d5d5fa8ac94b984
GitHub Action <action@github.com>
2021-08-10 08:57:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/tiansan/mvvmhilttemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/tiansan/mvvmhilttemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.tiansan.mvvmhilttemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tiansan.mvvmhilttemplate.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/tiansan/mvvmhilttemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/tiansan/mvvmhilttemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.tiansan.mvvmhilttemplate.services
 
+import com.github.tiansan.mvvmhilttemplate.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
